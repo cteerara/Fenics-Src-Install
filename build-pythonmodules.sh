a59d0e2d738915da4db5eb35642461ed5e9e0881
Chayut Teeraratkul <cteerara@u.rochester.edu>
2019-11-12 20:09:51 -0700

    Fixed boost dependency

--- build-pythonmodules.sh
+++ build-pythonmodules.sh
@@ -8,6 +8,7 @@ python3 -m pip install --upgrade --prefix=${PREFIX} --no-cache-dir --ignore-inst
 python3 -m pip install --upgrade --prefix=${PREFIX} --no-cache-dir --ignore-installed setuptools 
 python3 -m pip install --upgrade --prefix=${PREFIX} --no-cache-dir --ignore-installed sympy
 python3 -m pip install --upgrade --prefix=${PREFIX} --no-cache-dir --ignore-installed h5py numexpr
+python3 -m pip install --upgrade --prefix=${PREFIX} --no-cache-dir --ignore-installed setuptools 
 python3 -m pip install --upgrade --prefix=${PREFIX} --no-cache-dir --ignore-installed mpi4py
 python3 -m pip install --prefix=${PREFIX} --no-cache-dir https://bitbucket.org/petsc/petsc4py/downloads/petsc4py-${PETSC_VERSION}.tar.gz
 python3 -m pip install --prefix=${PREFIX} --no-cache-dir https://bitbucket.org/slepc/slepc4py/downloads/slepc4py-${SLEPC_VERSION}.tar.gz
